113bc3a7592f1263dea9cdc29dc6f67e73cb0cc9
Jared <145057449+JaredB990@users.noreply.github.com>
2023-10-25 13:46:02 -0400

    First refactoring of the UI

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class CybersecurityNewsApp:
         self.root = root
         self.root.title("Cybersecurity News Filter")
 
-        # User input field for filtering
+        # User input field of UI
         self.filter_entry = tk.Entry(root, width=40)
         self.filter_entry.grid(row=0, column=0, padx=10, pady=10, columnspan=2)
         self.filter_entry.insert(0, "Enter keywords to filter news")
@@ -15,11 +15,11 @@ class CybersecurityNewsApp:
         filter_button = tk.Button(root, text="Filter", command=self.filter_news)
         filter_button.grid(row=0, column=2, padx=10, pady=10)
 
-        # Window to display filtered news articles
+        # Display area for RSS news information
         self.news_list = tk.Listbox(root, width=60, height=15)
         self.news_list.grid(row=1, column=0, columnspan=3, padx=10, pady=10)
 
-        # Label for the list
+        # Label for the filtered RSS news information
         label = tk.Label(root, text="Filtered News")
         label.grid(row=2, column=0, columnspan=3)
 
@@ -27,18 +27,19 @@ class CybersecurityNewsApp:
         keywords = self.filter_entry.get()
         self.news_list.delete(0, tk.END)
         
-        # Uses feedparser to go to the RSS feed we want to get information from.
+        # Uses feedparser to extract the data we want from the RSS feed we want to get information from
         rss_feed = feedparser.parse('https://www.cisa.gov/news.xml')
 
         for entry in rss_feed.entries:
             title = entry.title
+            summary = entry.summary
+            pub_date = entry.published
             link = entry.link
 
-            # Access the publication date
-            pub_date = entry.published
-
-            if keywords.lower() in title.lower() or keywords.lower():
+            # Adds information we wanted from the RSS feed and displays it in the UI
+            if keywords.lower() in title.lower() or keywords.lower() in summary.lower():
                 self.news_list.insert(tk.END, title)
+                self.news_list.insert(tk.END, f"Summary: {summary}")
                 self.news_list.insert(tk.END, f"Published: {pub_date}")
                 self.news_list.insert(tk.END, f"Link: {link}")
                 self.news_list.insert(tk.END, '')
